b9280a60fab67842a192b44e28524a3d8d2ea306
prismsuwal <prismsuwal@gmail.com>
2021-02-01 16:09:46 +0545

    Minor changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,26 +7,26 @@ function Navbar() {
     <nav className="navbar navbar-expand-lg navbar-light">
       <div className='container'>
         <div className="container-fluid">
-          <a className="navbar-brand" href="#"><img className='logo' src={logo} alt='PRISM' /></a>
+          <a className="navbar-brand" href="/"><img className='logo' src={logo} alt='PRISM' /></a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item" active>
-                <a className="nav-link" href="#">Home<span className='sr-only'>(current)</span></a>
+                <a className="nav-link" href="/">Home<span className='sr-only'>(current)</span></a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="">About me</a>
+                <a className="nav-link" href="/">About me</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="">Projects</a>
+                <a className="nav-link" href="/">Projects</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="">Technologies</a>
+                <a className="nav-link" href="/">Technologies</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="">Contacts</a>
+                <a className="nav-link" href="/">Contacts</a>
               </li>
             </ul>
           </div>
