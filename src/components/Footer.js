6c068048d172e62a23b3cad395a66a78241b1223
prismsuwal <prismsuwal@gmail.com>
2021-03-09 14:44:23 +0545

    Footer_Changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,17 +9,17 @@ function Footer() {
           <div className='input-areas'>
             <form>
               <input type='email' name='email' placeholder='Your email' className='footer-input'/>
-              <br></br><input type='email' name='text' placeholder='Your message' className='footer-input'/>
+              <br></br><input type='text' name='message' placeholder='Your message' className='footer-input'/>
               <br></br><button buttonStyle='btn--outline'>Send</button>
             </form>
           </div>       
       </div>
       <div className='social-media-wrap'>
         <div className='social-icons'>
-          <a className='social-icon-link facebook'  aria-label='Facebook' href='https://www.facebook.com/suwal.prism/'><i class="fab fa-facebook fa-2x"></i></a>
-          <a className='social-icon-link instagram' aria-label='Instagram' href='https://www.instagram.com/prism_suwal/'><i class="fab fa-instagram fa-2x"></i></a>
-          <a className='social-icon-link github' aria-label='Github' href='https://github.com/PrizmSuwal'><i class="fab fa-github fa-2x"></i></a>
-          <a className='social-icon-link linkedin' aria-label='Linkedin' href='https://www.linkedin.com/in/prism-suwal-8741b4192/'><i class="fab fa-linkedin fa-2x"></i></a>
+          <a className='social-icon-link facebook'  aria-label='Facebook' href='https://www.facebook.com/suwal.prism/' target='_blank'><i class="fab fa-facebook fa-2x"></i></a>
+          <a className='social-icon-link instagram' aria-label='Instagram' href='https://www.instagram.com/prism_suwal/' target='_blank'><i class="fab fa-instagram fa-2x"></i></a>
+          <a className='social-icon-link github' aria-label='Github' href='https://github.com/PrizmSuwal' target='_blank'><i class="fab fa-github fa-2x"></i></a>
+          <a className='social-icon-link linkedin' aria-label='Linkedin' href='https://www.linkedin.com/in/prism-suwal-8741b4192/' target='_blank'><i class="fab fa-linkedin fa-2x"></i></a>
         </div>
         <small className='website-rights'> © 2021, Prism Suwal. All Rights Reserved.</small> 
       </div>
