4f4337be1a2b5f6c3f8373c5f10b66eebe686369
prismsuwal <prismsuwal@gmail.com>
2021-03-16 13:47:50 +0545

    Toggler_Fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ function Navbar() {
       <div className='container'>
         <div className="container-fluid">
           <a className="navbar-brand" href="/"><img className='logo' src={logo} alt='PRISM' /></a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
