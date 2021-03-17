ffaa202d7d9fc00d0f937ee217a0358cd74efa47
prismsuwal <prismsuwal@gmail.com>
2021-03-17 12:09:42 +0545

    Body_Modified

--- src/components/Body.js
+++ src/components/Body.js
@@ -21,29 +21,39 @@ function Body() {
         <div className='subContent-Technologies'>
           <h4>Technologies</h4>
           <div className='technologies'>
-            <div className='technologyImg'>
-              <img className='img-technology' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/180px-HTML5_logo_and_wordmark.svg.png' alt='HTML'></img>
+            <div className='technology-row-top'>
+              <div className='technologyImg'>
+                <img className='img-technology' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/180px-HTML5_logo_and_wordmark.svg.png' alt='HTML'></img>
+              </div>
+              <div className='technologyImg'>
+                <img className='img-technology right' src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/120px-CSS3_logo_and_wordmark.svg.png' alt='CSS'></img>
+              </div>
+              <div className='technologyImg'>
+                <img className='img-technology' src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/220px-Node.js_logo.svg.png' alt='NodeJS' ></img>
+              </div>
+              <div className='technologyImg'>
+                <img className='img-technology' src='data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K' alt='React JS'></img>
+              </div>
+              <div className='technologyImg'>
+                <img className='img-technology' src='https://upload.wikimedia.org/wikipedia/en/thumb/4/45/MongoDB-Logo.svg/375px-MongoDB-Logo.svg.png' alt='MongoDB'></img>
+              </div>
+              <div className='technologyImg'>
+                <img className='img-technology' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Expressjs.png/330px-Expressjs.png' alt='Express JS'></img>
+              </div>
             </div>
-            <div className='technologyImg'>
-              <img className='img-technology' src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/120px-CSS3_logo_and_wordmark.svg.png' alt='CSS'></img>
-            </div>
-            <div className='technologyImg'>
-              <img className='img-technology' src='https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/JQuery_logo.svg/330px-JQuery_logo.svg.png' alt='Jquery'></img>
-            </div>
-            <div className='technologyImg'>
-              <img className='img-technology' src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/220px-Node.js_logo.svg.png' alt='NodeJS' ></img>
-            </div>
-            <div className='technologyImg'>
-              <img className='img-technology' src='data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K' alt='React JS'></img>
-            </div>
-            <div className='technologyImg'>
-              <img className='img-technology' src='https://upload.wikimedia.org/wikipedia/en/thumb/4/45/MongoDB-Logo.svg/375px-MongoDB-Logo.svg.png' alt='MongoDB'></img>
-            </div>
-            <div className='technologyImg'>
-              <img className='img-technology' src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/150px-PHP-logo.svg.png' alt='PHP'></img>
-            </div>
-            <div className='technologyImg'>
-              <img className='img-technology' src='https://upload.wikimedia.org/wikipedia/en/thumb/e/ee/MySQL_Logo.png/104px-MySQL_Logo.png' alt='MySql'></img>
+            <div className='technology-row-bottom'>
+              <div className='technologyImg'>
+                <img className='img-technology' src='https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/JQuery_logo.svg/330px-JQuery_logo.svg.png' alt='Jquery'></img>
+              </div>
+              <div className='technologyImg'>
+                <img className='img-technology bottom' src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/150px-PHP-logo.svg.png' alt='PHP'></img>
+              </div>
+              <div className='technologyImg'>
+                <img className='img-technology' src='https://camo.githubusercontent.com/2eb688a747805c9acd144faf728c8a30f86fc4ca5fb39e6528232f0372151364/68747470733a2f2f63646e2e7261776769742e636f6d2f7075676a732f7075672d6c6f676f2f656563343336636565386664396431373236643738333963626539396431663639343639326330632f5356472f7075672d66696e616c2d6c6f676f2d5f2d636f6c6f75722d3132382e737667' alt='Pug JS'></img>
+              </div>
+              <div className='technologyImg'>
+                <img className='img-technology bottom' src='https://upload.wikimedia.org/wikipedia/en/thumb/e/ee/MySQL_Logo.png/104px-MySQL_Logo.png' alt='MySql'></img>
+              </div>
             </div>
           </div>
         </div>
