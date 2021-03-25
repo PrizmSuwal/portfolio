16d8f9075cfbdd4a34fb757f9e2487ca587c7f04
prismsuwal <prismsuwal@gmail.com>
2021-03-25 18:14:15 +0545

    Folder_Structure_Changed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Body from './components/Body';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import About from './components/Aboutme/Aboutme';
+import Projects from './components/Projects/Projects';
+import Technologies from './components/Technologies/Technologies';
+
 function App() {
   return (
     <>
       <Navbar />
-      <Body />
+      <Home />
+      <About />
+      <Projects/>
+      <Technologies />
       <Footer />
     </>
   );
