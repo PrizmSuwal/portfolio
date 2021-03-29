29ce236ccaf8e884cc12942490f1411d69aee901
prismsuwal <prismsuwal@gmail.com>
2021-03-29 11:39:09 +0545

    Folder_Structure_Changed

---
+++ src/components/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../Body.css';
+import Animation from '../../495px-Light_dispersion_conceptual_waves.gif';
+
+
+function Home() {
+  return (
+    <div id='home'>
+      <div className='content'>
+        <div className='subContent'>
+          <h6>Stay tuned</h6>
+          <img className='img-underConstruction' src ={Animation} alt='Under Construction' />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
