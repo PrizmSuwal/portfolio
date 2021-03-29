17a0a090bd546a68fcc4cba2c65abcd43c5edc3f
prismsuwal <prismsuwal@gmail.com>
2021-03-30 01:04:19 +0545

    Folder_Structure_Changed

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../Body.css';
+
+function Projects() {
+  return (
+    <div id= 'projects'>
+      <div className='content'>
+        <div className='subContent-Projects'>
+          <h4>Ongoing Projects</h4>
+          <img className='projectLogo' src='http://xinney.com/img/logo.png' alt='Xinney logo' />
+          <img className='projectLogo' src='http://174.138.38.24:8572/images/logo.svg' alt='Doctor Saab' />
+          <br></br>
+          <img className='projectLogoDeckdo' src='https://avatars.githubusercontent.com/u/52381212?s=400&u=6e818e15326ee39a120f50f66aad51e398d53ad1&v=4' alt='DeckDo Inc.'></img>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Projects
