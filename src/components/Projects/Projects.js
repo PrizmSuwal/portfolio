183454e239c0f95405b568b7c48fb0f91ca73eac
prismsuwal <prismsuwal@gmail.com>
2021-04-06 13:48:15 +0545

    Picture_added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import DoctorSaab from '../../photos/DrSaab.svg';
+import Xinney from '../../photos/Xinney.png';
 import './Projects.css';
 
 function Projects() {
@@ -7,10 +9,13 @@ function Projects() {
       <div className='content'>
         <div className='subContent-Projects'>
           <h4>Ongoing Projects</h4>
-          <img className='projectLogo' src='http://xinney.com/img/logo.png' alt='Xinney logo' />
-          <img className='projectLogo' src='http://174.138.38.24:8572/images/logo.svg' alt='Doctor Saab' />
-          <br></br>
-          <img className='projectLogoDeckdo' src='https://avatars.githubusercontent.com/u/52381212?s=400&u=6e818e15326ee39a120f50f66aad51e398d53ad1&v=4' alt='DeckDo Inc.'></img>
+          <div className='upperProject'>
+            <img className='projectLogo-Xinney' src={Xinney} alt='Xinney logo' />
+            <img className='projectLogo-DrSaab' src={DoctorSaab} alt='Doctor Saab' />
+          </div>
+          <div className='upperProject'>
+            <img className='projectLogo-Deckdo' src='https://avatars.githubusercontent.com/u/52381212?s=400&u=6e818e15326ee39a120f50f66aad51e398d53ad1&v=4' alt='DeckDo Inc.'></img>
+          </div>
         </div>
       </div>
     </div>
