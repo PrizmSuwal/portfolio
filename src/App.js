1dad11783509df2b7ea2130a59f57f7750a58c36
prismsuwal <prismsuwal@gmail.com>
2021-04-14 13:22:43 +0545

    Contact_Me

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './components/Home/Home';
 import About from './components/Aboutme/Aboutme';
 import Projects from './components/Projects/Projects';
 import Technologies from './components/Technologies/Technologies';
+import Contact from './components/Contact/Contact';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <About />
       <Projects/>
       <Technologies />
+      <Contact />
       <Footer />
     </>
   );
