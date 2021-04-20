6d393d571fbbe50c4dfaeff85b5b52aac9ab7fde
prismsuwal <prismsuwal@gmail.com>
2021-04-20 08:23:05 +0545

    Footer_Modified

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,10 +11,11 @@ function Footer() {
         </a>
         <p>Leveling up one day at a time. </p>
         <div className='social-icons'>
-          <a className='social-icon-link facebook'  aria-label='Facebook' href='https://www.facebook.com/suwal.prism/' target='_blank' rel='noreferrer'><i class='fab fa-facebook fa-3x'></i></a>
+          <a className='social-icon-link facebook'  aria-label='Facebook' href='https://www.facebook.com/suwal.prism/' target='_blank' rel='noreferrer'><i class='fab fa-facebook-f fa-3x'></i></a>
           <a className='social-icon-link instagram' aria-label='Instagram' href='https://www.instagram.com/prism_suwal/' target='_blank' rel='noreferrer'><i class='fab fa-instagram fa-3x'></i></a>
+          <a className='social-icon-link gmail' aria-label='Gmail' href='mailto:prizmsuwal@gmail.com' target='_blank' rel='noreferrer'><i class='fas fa-envelope fa-3x'></i></a>
           <a className='social-icon-link github' aria-label='Github' href='https://github.com/PrizmSuwal' target='_blank' rel='noreferrer'><i class='fab fa-github fa-3x'></i></a>
-          <a className='social-icon-link linkedin' aria-label='Linkedin' href='https://www.linkedin.com/in/prism-suwal-8741b4192/' target='_blank' rel='noreferrer'><i class='fab fa-linkedin fa-3x'></i></a>
+          <a className='social-icon-link linkedin' aria-label='Linkedin' href='https://www.linkedin.com/in/prism-suwal-8741b4192/' target='_blank' rel='noreferrer'><i class='fab fa-linkedin-in fa-3x'></i></a>
         </div>
         <small className='website-rights'> © 2021, Prism Suwal. All Rights Reserved.</small> 
       </div>
